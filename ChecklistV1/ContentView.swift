93dc5adf80550abc1e7dbec02d9e39ce0aa40df0
hwangdongjun <wbsl0427@gmail.com>
2021-05-30 17:50:35 +0900

    Initial Commit

---
+++ ChecklistV1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ChecklistV1
+//
+//  Created by 황동준 on 2021/05/30.
+//  Copyright © 2021 황동준. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
