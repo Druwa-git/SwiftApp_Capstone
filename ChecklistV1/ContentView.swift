972d7c859550e621dc6fc9065ec03c665616e9ea
hwangdongjun <wbsl0427@gmail.com>
2021-05-30 17:53:40 +0900

    n=make initial list view

--- ChecklistV1/ContentView.swift
+++ ChecklistV1/ContentView.swift
@@ -10,7 +10,13 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        List {
+            Text("My name is")
+            Text("Hwang Dong Jun")
+            Text("everyone please")
+            Text("make a noise")
+            Text("yeah!!!")
+        } // end of List
     }
 }
 
