97b7bcc21bdedde4bda05b9810ea4412f9165f5c
hwangdongjun <wbsl0427@gmail.com>
2021-06-06 02:40:12 +0900

    change the starting scene

---
+++ ChecklistV1/EditChecklistItemView.swift
@@ -0,0 +1,21 @@
+//
+//  EditChecklistItemView.swift
+//  ChecklistV1
+//
+//  Created by 황동준 on 2021/06/06.
+//  Copyright © 2021 황동준. All rights reserved.
+//
+
+import SwiftUI
+
+struct EditChecklistItemView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct EditChecklistItemView_Previews: PreviewProvider {
+    static var previews: some View {
+        EditChecklistItemView()
+    }
+}
