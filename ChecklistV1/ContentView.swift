d1e70bddc040e9a2dfb10e094f71f457f03e1e52
hwangdongjun <wbsl0427@gmail.com>
2021-06-06 01:58:43 +0900

    show checkable item

--- ChecklistV1/ContentView.swift
+++ ChecklistV1/ContentView.swift
@@ -7,15 +7,37 @@
 //
 
 import SwiftUI
+struct ChecklistItem {
+    var name: String
+    var isChecked: Bool = false
+}
 
 struct ContentView: View {
-    @State var checklistItems = ["Do Capstone Design 1 Homework", "Wash dishes", "Do Capstone Design 2 Homework", "Watch Lecture 11", "Hang Out With Girlfriends", "Watch Lecture 12"]
+    //@State var checklistItems = ["Do Capstone Design 1 Homework", "Wash dishes", "Do Capstone Design 2 Homework", "Watch Lecture 11", "Hang Out With Girlfriends", "Watch Lecture 12"]
+    @State var checklistItems = [
+        ChecklistItem(name : "Do Capstone Design 1 Homework"),
+        ChecklistItem(name : "Wash dishes"),
+        ChecklistItem(name : "Do Capstone Design 2 Homework", isChecked: true),
+        ChecklistItem(name : "Watch Lecture 11"),
+        ChecklistItem(name : "Hang Out With Girlfriends"),
+        ChecklistItem(name : "Watch Lecture 12", isChecked: true),
+    ]
     
     var body: some View {
         NavigationView {
             List {
-                ForEach(checklistItems, id: \.self){
-                    item in Text(item)
+                ForEach(checklistItems, id: \.self.name){
+                    checklistItems in
+                    HStack{
+                        Text(checklistItems.name)
+                        Spacer()
+                        if checklistItems.isChecked {
+                            Text("✔️")
+                        }
+                        else{
+                            Text("⬛️")
+                        }
+                    }
                 } //end of foreach
                 .onDelete(perform: deleteListItem)
                 .onMove(perform: moveListItem)
