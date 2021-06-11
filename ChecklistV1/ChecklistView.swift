9f247d5e997bf905aa5bc11b56855e17d5d67f72
hwangdongjun <wbsl0427@gmail.com>
2021-06-12 00:53:55 +0900

    add item mark; plus.circle fill

--- ChecklistV1/ChecklistView.swift
+++ ChecklistV1/ChecklistView.swift
@@ -13,6 +13,7 @@ struct ChecklistView: View {
     //@State var checklistItems = ["Do Capstone Design 1 Homework", "Wash dishes", "Do Capstone Design 2 Homework", "Watch Lecture 11", "Hang Out With Girlfriends", "Watch Lecture 12"]
     
     @ObservedObject var checklist = Checklist()
+    @State var newChecklistItemViewIsVisible = false
     
     var body: some View {
         NavigationView {
@@ -38,7 +39,14 @@ struct ChecklistView: View {
                     .onDelete(perform: checklist.deleteListItem)
                     .onMove(perform: checklist.moveListItem)
             } // end of List
-            .navigationBarItems(trailing: EditButton())
+            .navigationBarItems(
+                leading: Button(action: {self.newChecklistItemViewIsVisible = true}) {
+                    HStack {
+                        Image(systemName: "plus.circle.fill")
+                        Text("Add Item")
+                    }
+                },
+                trailing: EditButton())
             .navigationBarTitle("CheckList")
                 .onAppear(){
                     self.checklist.printChecklistContents()
